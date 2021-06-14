699fefd329109bd947c86ecf460eaaf4c7205b68
Ursula Shaw <ursula.e.shaw@gmail.com>
2021-06-13 21:00:02 -0400

    AnagramChecker class

---
+++ src/main/java/ex24/base/AnagramChecker.java
@@ -0,0 +1,24 @@
+package ex24.base;
+
+import java.util.Arrays;
+
+public class AnagramChecker {
+
+    private static String reorder(String string) {
+        String str = string.toLowerCase();
+        str = str.replaceAll(" ", "");
+        char[] characterArray = str.toCharArray();
+        Arrays.sort(characterArray);
+        return (new String(characterArray));
+    }
+
+    public String isAnagram(String string1, String string2) {
+        if (reorder(string1).length() != reorder(string2).length()) {
+            return String.format("%s and %s are not the same length. They are not anagrams.", string1, string2);
+        } else if(reorder(string1).equals(reorder(string2))) {
+            return String.format("%s and %s are anagrams.", string1, string2);
+        } else {
+            return String.format("%s and %s are not anagrams.", string1, string2);
+        }
+    }
+}
